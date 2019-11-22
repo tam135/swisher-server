5d178927daea1ee102fcdf53c2bd42feaf553f67
Tam Phan <tnphan0204@gmail.com>
2019-11-22 17:09:40 -0500

    created sign up endpoint

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  swishes: [
+    {
+      userHandle: "user",
+      body: "this is a swish body",
+      createdAt: "2019-11-22T21:44:18.861Z",
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,27 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');  
 const serviceAccount = require('./admin.json')
 
-admin.initializeApp( {
-    credential: admin.credential.cert(serviceAccount)
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
 });
 
 const express = require('express');
 const app = express();
+const firebase = require('firebase')
+
+
+var config = {
+  apiKey: "AIzaSyAyj0b1aa_5b6535LvrS8SzaCqwVjR72oQ",
+  authDomain: "swisher-ce70b.firebaseapp.com",
+  databaseURL: "https://swisher-ce70b.firebaseio.com",
+  projectId: "swisher-ce70b",
+  storageBucket: "swisher-ce70b.appspot.com",
+  messagingSenderId: "26849947657",
+  appId: "1:26849947657:web:5c645723ea6adb3202c8d8",
+  measurementId: "G-5B18VESC3K"
+};
+
+firebase.initializeApp(config);
 
 app.get('/swishes', (req, res) => {
     admin
@@ -37,7 +52,7 @@ app.post('/swish', (req, res) => {
   const newSwish = {
       body: req.body.body,
       userHandle: req.body.userHandle,
-      createdAt: admin.firestore.Timestamp.fromDate(new Date())
+      createdAt: new Date().toISOString()
   };
 
   admin.firestore()
@@ -51,5 +66,26 @@ app.post('/swish', (req, res) => {
         console.log(err)
     })
 });
+
+// Sign up route
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle
+    };
+
+    // TODO: validate data
+
+    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then(data => {
+            return res.status(201).json({ message: `user ${data.user.uid} signed up successfully` })
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        })
+})
 
 exports.api = functions.https.onRequest(app);
