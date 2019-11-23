83fc7c9eeaf5f62468af40b84ddf4d40975d52d6
Tam Phan <tnphan0204@gmail.com>
2019-11-23 00:41:48 -0500

    user/email validation

--- functions/index.js
+++ functions/index.js
@@ -24,9 +24,10 @@ var config = {
 
 firebase.initializeApp(config);
 
+const db = admin.firestore()
+
 app.get('/swishes', (req, res) => {
-    admin
-      .firestore()
+    db
       .collection("swishes")
       .orderBy('createdAt', 'desc')
       .get()
@@ -77,15 +78,46 @@ app.post('/signup', (req, res) => {
     };
 
     // TODO: validate data
-
-    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
-        .then(data => {
-            return res.status(201).json({ message: `user ${data.user.uid} signed up successfully` })
+    let token, userId;
+    db.doc(`/users/${newUser.handle}`).get()
+        .then(doc => {
+            if(doc.exists) {
+                return res.status(400).json({ handle: 'this handle is already taken'}) 
+            } else {
+                return firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(newUser.email, newUser.password)          
+            }
         })
+        .then(data => {
+            userId = data.user.uid;
+            return data.user.getIdToken();
+        })
+        .then(token => {
+            token = token;
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId
+            };
+            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+        })
+            .then(() => {
+                return res.status(201).json({ token });
+            })
         .catch(err => {
             console.error(err);
-            return res.status(500).json({ error: err.code });
+            if (err.code === "auth/email-already-in-use") {
+                return res.status(400).json({ email: 'Email is already in use' })
+            } else {
+                return res.status(500).json({ error: err.code });
+            }
+              
         })
+
+    
 })
 
 exports.api = functions.https.onRequest(app);
+//eyJhbGciOiJSUzI1NiIsImtpZCI6IjhhMzY5M2YxMzczZjgwYTI1M2NmYmUyMTVkMDJlZTMwNjhmZWJjMzYiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vc3dpc2hlci1jZTcwYiIsImF1ZCI6InN3aXNoZXItY2U3MGIiLCJhdXRoX3RpbWUiOjE1NzQ0ODczMDQsInVzZXJfaWQiOiJ2QzdvYXMyR2xPTkdiRE8zcjVuTEduaURqQzcyIiwic3ViIjoidkM3b2FzMkdsT05HYkRPM3I1bkxHbmlEakM3MiIsImlhdCI6MTU3NDQ4NzMwNCwiZXhwIjoxNTc0NDkwOTA0LCJlbWFpbCI6InVzZXIyQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJ1c2VyMkBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.QkPsoh5iUumRy4u3NowTrE4WXqtEnLJbPywsglV2TuUibHTC7bMHa1hE_utNouiJghm0a-baMQO2b7H8TBuf5JvN9eFvsROCBP06Z-7p4VYbNMVt1wvd3lwPJ5AFGNMPI5-LMQdqX6mc7UFo4PxShAIMNlqmaeFEet-nS1nUHSH7x1asrfKNlVeoSzKAevfNhYVq6kQwkOy8PIYd3_R-b4aMzBGTmyau8dk-mZL_VBkyyo1u66rNcUQxqlA17-9nhwqgsthWBnn1tDptsXK13ozu2JZ8xmePX8GehRTPbJKVKyyo24ezuhW_7tkro9uNSKwos0BI-IzvTaRFO1ZR8Q
