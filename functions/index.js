9184a6dd213ab5f82180bdd5fb5f5d017e048df5
Tam Phan <tnphan0204@gmail.com>
2019-11-25 23:06:40 -0500

    finished validation for sign up route

--- functions/index.js
+++ functions/index.js
@@ -68,6 +68,23 @@ app.post('/swish', (req, res) => {
     })
 });
 
+const isEmail = (email) => {
+    const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(email.match(emailRegEx)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const isEmpty = (str) => {
+    if(str.trim() === ''){
+        return true;
+    } else {
+        return false;
+    }
+}
 // Sign up route
 app.post('/signup', (req, res) => {
     const newUser = {
@@ -76,6 +93,31 @@ app.post('/signup', (req, res) => {
         confirmPassword: req.body.confirmPassword,
         handle: req.body.handle
     };
+    
+
+    let errors = {};
+
+    if(isEmpty(newUser.email)){
+        errors.empty = 'Must not be empty'
+    } else if (!isEmail(newUser.email)) {
+        errors.email = 'Must be a valid email address'
+    } 
+
+    if(isEmpty(newUser.password)) {
+        errors.password = 'Must not be empty'
+    }
+
+    if(newUser.password !== newUser.confirmPassword) {
+        errors.confirmPassword = 'Passwords must match'
+    }
+
+    if (isEmpty(newUser.handle)) {
+      errors.handle = "Must not be empty";
+    }
+
+    if(Object.keys(errors).length > 0) {
+        res.status(400).json(errors)
+    }
 
     // TODO: validate data
     let token, userId;
