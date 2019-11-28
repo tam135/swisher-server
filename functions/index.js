3f4cf05e85e9b6821a106f4765eccb9e46918ad4
Tam Phan <tnphan0204@gmail.com>
2019-11-28 09:57:53 -0500

    refactored

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,12 @@
+const express = require("express");
+const app = express();
 const functions = require('firebase-functions');
 
 const { getAllSwishes, postOneSwish } = require('./handlers/swishes')
-const { signup, login } = require('./handlers/users')
+const { signup, login, uploadImage } = require('./handlers/users')
 
-const express = require('express');
-const app = express();
+
 const FBAuth = require('./util/fbAuth');
-
 
 
 // Swishes route
@@ -16,6 +16,7 @@ app.post("/swish", FBAuth, postOneSwish);
 // Users routes
 app.post('/signup', signup)
 app.post('/login', login)
+app.post('/user/image', FBAuth, uploadImage)
 
 
 
